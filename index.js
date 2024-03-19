9d886fbeea426f83947b31fbefde30ca17a18e82
JungHun Yang <66667038+yjh96080@users.noreply.github.com>
2024-03-19 22:27:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('양정훈의 퇴근(예정):17시 28분 23초 ! ')
+  res.send(' 퇴근(예정):17시 28분 23초 ! ')
 })
 
 app.listen(3000);
